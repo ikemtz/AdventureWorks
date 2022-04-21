229cdd8ecd8097b3675648969508df7c9ae831db
IkeMtz <ikemtz@outlook.com>
2022-04-21 19:57:56 -0400

    Adding OrderLineItem Controller

---
+++ src/IkeMtz.AdventureWorks.WebApi.Tests/Unigration/OrderLineItemsTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using IkeMtz.AdventureWorks.Data;
+using IkeMtz.AdventureWorks.Models;
+using IkeMtz.AdventureWorks.Tests;
+using IkeMtz.NRSRx.Core.Unigration;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IkeMtz.AdventureWorks.WebApi.Tests.Unigration
+{
+  [TestClass]
+  public partial class OrderLineItemsTests : BaseUnigrationTests
+  {
+
+    [TestMethod]
+    [TestCategory("Unigration")]
+    public async Task DeleteOrderLineItemTest()
+    {
+      var orderLineItem = Factories.OrderLineItemFactory(); 
+      using var srv = new TestServer(TestHostBuilder<Startup, UnigrationWebApiTestStartup>()
+        .ConfigureTestServices(x =>
+        {
+          ExecuteOnContext<DatabaseContext>(x, db =>
+          {
+            _ = db.OrderLineItems.Add(orderLineItem);
+          });
+        }));
+      var client = srv.CreateClient();
+      GenerateAuthHeader(client, GenerateTestToken()); 
+
+      var resp = await client.DeleteAsync($"api/v1/{nameof(OrderLineItem)}s.json?id={orderLineItem.Id}");
+      _ = resp.EnsureSuccessStatusCode(); 
+
+      var dbContext = srv.GetDbContext<DatabaseContext>();
+      var dbOrderLineItem = await dbContext.Orders.FirstOrDefaultAsync(t => t.Id == orderLineItem.Id);
+
+      Assert.IsNull(dbOrderLineItem);
+    }
+  }
+}
---
+++ src/IkeMtz.AdventureWorks.WebApi/Controllers/V1/OrderLineItemsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using IkeMtz.AdventureWorks.Data;
+using IkeMtz.AdventureWorks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace IkeMtz.AdventureWorks.WebApi.Controllers.V1
+{
+  [Route("api/v{version:apiVersion}/[controller].{format}"), FormatFilter]
+  [ApiVersion(VersionDefinitions.v1_0)]
+  [ApiController]
+  public class OrderLineItemsController : ControllerBase
+  {
+    private readonly DatabaseContext _databaseContext;
+    public OrderLineItemsController(DatabaseContext databaseContext)
+    {
+      _databaseContext = databaseContext;
+    }
+     
+    // Put api/Items
+    [HttpDelete]
+    [ProducesResponseType(Status200OK, Type = typeof(Order))]
+    public async Task<ActionResult> Delete([FromQuery] Guid id)
+    {
+      var obj = await _databaseContext.OrderLineItems.FirstOrDefaultAsync(t => t.Id == id)
+        .ConfigureAwait(false);
+      if (obj == null)
+      {
+        return Conflict($"OrderLineItem Id {id} is not found");
+      }
+      else
+      {
+        _ = _databaseContext.Remove(obj);
+        _ = await _databaseContext.SaveChangesAsync()
+            .ConfigureAwait(false);
+        return Ok(obj);
+      }
+    }
+  }
+}
